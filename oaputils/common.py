7f2a06b6835544c0d28997e4f7e40369bcbe1adc
wanwei3 <wei.f.wang@daimler.com>
2020-07-01 17:23:30 +0800

    adjust password length to 16

--- oaputils/common.py
+++ oaputils/common.py
@@ -17,11 +17,11 @@ def gen_password(count=1):
     src_special = "!@#$%^&*()_+-=[]{}|'"
     list_passwds = []
     for i in range(int(count)):
-        list_passwd_all = random.sample(src, 8)  # 从字母和数字中随机取8位
-        list_passwd_all.extend(random.sample(string.digits, 1))  # 让密码中一定包含数字
+        list_passwd_all = random.sample(src, 10)  # 从字母和数字中随机取8位
+        list_passwd_all.extend(random.sample(string.digits, 2))  # 让密码中一定包含数字
         list_passwd_all.extend(random.sample(string.ascii_lowercase, 1))  # 让密码中一定包含小写字母
         list_passwd_all.extend(random.sample(string.ascii_uppercase, 1))  # 让密码中一定包含大写字母
-        list_passwd_all.extend(random.sample(src_special, 1))  # 让密码中一定包含特殊字符
+        list_passwd_all.extend(random.sample(src_special, 2))  # 让密码中一定包含特殊字符
         random.shuffle(list_passwd_all)  # 打乱列表顺序
         str_passwd = ''.join(list_passwd_all)  # 将列表转化为字符串
         if str_passwd not in list_passwds:  # 判断是否生成重复密码
